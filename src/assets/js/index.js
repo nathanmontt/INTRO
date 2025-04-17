e89e28fbf12ef39e31133f17fe029128c6f85a5e
nathanmontt <contato.mont@gmail.com>
2025-04-17 08:17:16 -0300

    Modificação do estilo para comportar o estudo de JavaScript (7)

---
+++ src/assets/js/index.js
@@ -0,0 +1,5 @@
+console.log("Hello world");
+window.alert(`Eu gosto de pizza!`)
+
+document.getElementById('myP').textContent = `Texto teste para adicionar conteúdo por meio de JS`;
+/* ============================ */
