7fa3ddc0750af9ec3b5942ad11e9bd669db4563d
nathanmontt <contato.mont@gmail.com>
2025-04-17 09:52:15 -0300

    Adição do vigésimo sétimo conteúdo, em JavaScript

--- src/assets/js/index.js
+++ src/assets/js/index.js
@@ -1,5 +1,51 @@
 console.log("Hello world");
-window.alert(`Eu gosto de pizza!`)
+// window.alert(`Eu gosto de pizza!`);
 
 document.getElementById('myP').textContent = `Texto teste para adicionar conteúdo por meio de JS`;
+
+/* ============================ */
+
+let age = 25;
+console.log(`Você tem ${age} anos!`);
+console.log(`age é do tipo: ${typeof age}`);
+
+let price = 10.99;
+console.log(`O preço é de R$${price}`);
+console.log(`price é do tipo: ${typeof price}`);
+
+let gpa = 2.1
+console.log(`Nem sei o que é isso: ${gpa}`);
+console.log(`gpa é do tipo: ${typeof gpa}`);
+
+let firstName = "James";
+console.log(`Seu nome é: ${firstName}`);
+console.log(`firstName é do tipo: ${typeof firstName}`);
+
+let online = false;
+console.log(`Você está online? ${online}`);
+console.log(`online é do tipo: ${typeof online}`);
+
+let fullName = "Nathan Monteiro";
+let personAge = 25;
+let isPersonUniStudent = true;
+
+document.getElementById('personName').innerHTML = `&rightarrow; Seu nome é: ${fullName}. O tipo da variável fullName é <span class="specific-style">${typeof fullName}</span>;`;
+
+document.getElementById('personAge').innerHTML = `&rightarrow; Sua idade é: ${personAge} O tipo da variável personAge é <span class="specific-style">${typeof personAge}</span>;`;
+
+document.getElementById('isPersonStudent').innerHTML = `&rightarrow; É um estudante? ${isPersonUniStudent} O tipo da variável isPersonUniStudent é <span class="specific-style">${typeof isPersonUniStudent}</span>;`;
+
+/* ============================ */
+/* ============================ */
+/* ============================ */
+/* ============================ */
+/* ============================ */
+/* ============================ */
+/* ============================ */
+/* ============================ */
+/* ============================ */
+/* ============================ */
+/* ============================ */
+/* ============================ */
+/* ============================ */
 /* ============================ */
