d826438c2c1792fac3a9cbf5e6e047706a8f92d2
nathanmontt <contato.mont@gmail.com>
2025-04-19 22:57:13 -0300

    Adição do trigésimo sexto conteúdo, em JavaScript

--- src/assets/js/projects.js
+++ src/assets/js/projects.js
@@ -25,3 +25,31 @@ const btnRandom01 = document.getElementById('btn-random-01');
 btnRandom01.onclick = function () {
     random01.textContent = Math.round(Math.random() * 100) + 1;
 }
+
+/* CHECKED */
+
+const myCheckbox = document.getElementById('myCheckbox');
+const visaBtn = document.getElementById('visaBtn');
+const mastercardBtn = document.getElementById('mastercardBtn');
+const paypalBtn = document.getElementById('paypalBtn');
+const secondMySub = document.getElementById('secondMySub');
+const subResult = document.getElementById('subResult');
+const paymentResult = document.getElementById('paymentResult');
+
+secondMySub.onclick = function () {
+    if (myCheckbox.checked) {
+        subResult.textContent = `Você clicou!`;
+    } else {
+        subResult.textContent = `Você não clicou!`;
+    }
+
+    if (visaBtn.checked) {
+        paymentResult.textContent = `Você está pagando com Visa`;
+    } else if (mastercardBtn.checked) {
+        paymentResult.textContent = `Você está pagando com Mastercard`;
+    } else if (paypalBtn.checked) {
+        paymentResult.textContent = `Você está pagando com Paypal`;
+    } else {
+        paymentResult.textContent = `Selecione um tipo de pagamento!`;
+    }
+}
