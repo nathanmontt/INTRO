ad69ea10243f9d85c86a9e63557a3486c8e2e211
nathanmontt <contato.mont@gmail.com>
2025-04-21 16:12:41 -0300

    Adição do quadragésimo quinto conteúdo, em JavaScript

--- src/assets/js/index.js
+++ src/assets/js/index.js
@@ -307,6 +307,38 @@ console.log(entireLastName);
 // }
 
 /* ============================ */
+
+// const minNum = 1;
+// const maxNum = 10;
+// const answer = Math.floor(Math.random() * (maxNum - minNum + 1)) + minNum;
+
+// let attempts = 0;
+// let guess;
+// let running = true;
+
+// console.log(answer);
+
+// while (running) {
+//     guess = prompt(`Guess a number between ${minNum} and ${maxNum}`)
+//     guess = Number(guess);
+
+//     if(isNaN(guess)) {
+//         window.alert("Please enter a number!")
+//     } else if (guess < minNum || guess > maxNum) {
+//         window.alert("Please enter a valid number!")
+//     } else {
+//         attempts++;
+//         if (guess < answer) {
+//             window.alert("TOO LOW. TRY AGAIN!")
+//         } else if (guess > answer) {
+//             window.alert("TOO HIGH. TRY AGAIN!")
+//         } else {
+//             window.alert(`CORRECT! The number was ${answer} and it took you ${attempts} attempts!`);
+//             running = false;
+//         }
+//     }
+// }
+
 /* ============================ */
 /* ============================ */
 /* ============================ */
