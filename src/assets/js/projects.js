c2440a322d465100cd51ad2b958cb208e0a96abc
nathanmontt <contato.mont@gmail.com>
2025-04-21 23:49:01 -0300

    Projeto do curso JavaScript Full Course for free (2024), do Bro Code

--- src/assets/js/projects.js
+++ src/assets/js/projects.js
@@ -42,7 +42,7 @@ secondMySub.onclick = function () {
     } else {
         subResult.textContent = `Você não clicou!`;
     }
-
+    
     if (visaBtn.checked) {
         paymentResult.textContent = `Você está pagando com Visa`;
     } else if (mastercardBtn.checked) {
@@ -51,5 +51,31 @@ secondMySub.onclick = function () {
         paymentResult.textContent = `Você está pagando com Paypal`;
     } else {
         paymentResult.textContent = `Selecione um tipo de pagamento!`;
+    }
+}
+
+
+/* CONVERSION */
+
+const textbox = document.getElementById('textbox');
+const toFahrenheit = document.getElementById('toFahrenheit');
+const toCelsius = document.getElementById('toCelsius');
+const result = document.getElementById('result');
+let temperature;
+
+function convert () {
+    if (toFahrenheit.checked) {
+        temperature = Number(textbox.value);
+        temperature = temperature * 9 / 5 + 32
+        result.textContent = `${temperature.toFixed(1)}°F`;
+        
+    } else if (toCelsius.checked) {
+        temperature = Number(textbox.value);
+        temperature = (temperature + 32) * (5/9)
+
+        result.textContent = `${temperature.toFixed(1)}°C`;
+
+    } else {
+        result.textContent = "Selecione uma unidade!";
     }
 }
