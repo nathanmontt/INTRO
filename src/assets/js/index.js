fcd8cda792d535d0e632cc3ef5df2c6f8c4e681b
nathanmontt <contato.mont@gmail.com>
2025-04-24 17:38:28 -0300

    Adição do quinquagésimo quarto conteúdo, em JavaScript

--- src/assets/js/index.js
+++ src/assets/js/index.js
@@ -538,6 +538,15 @@ function formatDates(elements) {
 }
 
 /* ============================ */
+
+let n = [1,2,3,4,5,6,7];
+let evenNums = n.filter(isEven);
+console.log(evenNums);
+
+function isEven(el) {
+    return el%2 === 0;
+}
+
 /* ============================ */
 /* ============================ */
 /* ============================ */
