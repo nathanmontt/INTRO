d24886735d39b4c5a4ae36e8af1a24848cf4cfe5
nathanmontt <contato.mont@gmail.com>
2025-04-26 17:31:33 -0300

    Adição do sexagésimo terceiro conteúdo, em JavaScript

--- src/assets/js/index.js
+++ src/assets/js/index.js
@@ -739,4 +739,45 @@ console.log(user1.username);
 console.log(User.userCount);
 
 /* ============================ */
+
+class Animal {
+    alive = true;
+    eat() {
+        console.log(`This ${this.name} is eating`);
+    }
+    sleep() {
+        console.log(`This ${this.name} is sleeping`);
+    }
+}
+
+class Rabbit extends Animal {
+    name = "Rabbit";
+    run() {
+        console.log(`This ${this.name} is running`);
+    }
+}
+class Fish extends Animal {
+    name = "Fish";
+    swim() {
+        console.log(`This ${this.name} is swimming`);
+    }
+}
+class Dog extends Animal {
+    name = "Dog";
+}
+
+const rabbit = new Rabbit();
+const fish = new Fish();
+const dog = new Dog();
+
+console.log(rabbit.alive);
+rabbit.alive = false;
+console.log(rabbit.alive);
+
+rabbit.eat();
+rabbit.sleep();
+
+rabbit.run();
+fish.swim();
+
 /* ============================ */
